86cbfd664d8442dfa0aa96145d4b83d8337f3814
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-08-09 15:12:35 +0530

    [CHORE] Adding program version in bin

--- bin/ember-gen-uml.js
+++ bin/ember-gen-uml.js
@@ -3,6 +3,7 @@
 const program = require('commander');
 
 program
+.version('1.0.1')
 .usage('[path...] [options]')
 .description(
   'A CLI to generate PlantUML compatible UML diagrams for Ember Components'
