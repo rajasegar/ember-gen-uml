b1f5919591003330140d22d4934a796a7999da1e
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-08-09 15:02:47 +0530

    [CHORE] Use commander to convert to CLI
    Fixes #2

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  env: {
+    node: true,
+    es6: true
+  }
+};
---
+++ bin/ember-gen-uml.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+
+program
+.usage('[path...] [options]')
+.description(
+  'A CLI to generate PlantUML compatible UML diagrams for Ember Components'
+)
+.option('-o, --out <path>', 'Store uml assets (.pu files) in <path>')
+.option('--pods', 'Enable support for POD style components')
+.arguments('<path>')
+.parse(process.argv);
+
+const generate = require('../src/main');
+generate(program.args[0], program.out, program.pods)
+
+
