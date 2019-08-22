667aced9b105a6db8b28518553fdb2c40a8632d4
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-08-22 14:31:32 +0530

    [TESTS] Tests added for transform-typescript

--- src/transform-typescript.js
+++ src/transform-typescript.js
@@ -8,58 +8,13 @@ const kindMap = {
   '139': 'String'
 };
 
-let memberDefs = [];
-let serviceDefs = [];
-let extendDefs = [];
 
-let className = '';
-
-function traverse(node) {
-  switch(node.kind) {
-    case ts.SyntaxKind.ClassDeclaration:
-      //console.log(node.heritageClauses[0].types[0].expression.escapedText);
-      if(node.heritageClauses[0]) {
-        if(node.heritageClauses[0].types[0]) {
-          let _extends = node.heritageClauses[0].types[0].expression.text;
-
-          //let extension =  `${parent.name} <|-- ${componentName}`;
-          //extendDefs.push(extension);
-
-          if(_extends === "Component") {
-            node.members.filter(prop => prop.kind === KIND_PROPERTY_DECLARATION || prop.kind === KIND_METHOD_DECLARATION)
-              .forEach(prop => {
-                //console.log(prop.name.text);
-                let keyName = prop.name.text;
-                let isMethod = prop.kind === KIND_METHOD_DECLARATION;
-                let isService = prop.decorators && prop.decorators[0].expression.text === "service";
-                const scope = keyName.startsWith("_") ? "-" : "+";
-                if(isMethod) {
-                  memberDefs.push(`${scope}${keyName}()`);
-                } else if(isService) {
-                  let serviceDef =  `class ${prop.name.text} << (S, #FF7700) >>\n`;
-                  serviceDef += `${className} ..> ${prop.name.text} : service`;
-                  serviceDefs.push(serviceDef);
-                }
-                else {
-                  let type = prop.type ? kindMap[prop.type.kind] || 'any' : 'any';
-                  memberDefs.push(`${scope}${keyName} : ${type}`);
-                }
-
-
-              });
-          }
-        }
-      }
-
-      break;
-  }
-  ts.forEachChild(node, traverse);
-
-}
 
 function transform(fileName, code, componentName) {
 
-  className = componentName;
+  let memberDefs = [];
+  let serviceDefs = [];
+  let extendDefs = [];
 
 
   const ast = ts.createSourceFile(
@@ -84,9 +39,48 @@ class ${componentName} {
 
   return umlData;
 
+  function traverse(node) {
+    switch(node.kind) {
+      case ts.SyntaxKind.ClassDeclaration:
+        //console.log(node.heritageClauses[0].types[0].expression.escapedText);
+        if(node.heritageClauses[0]) {
+          if(node.heritageClauses[0].types[0]) {
+            let _extends = node.heritageClauses[0].types[0].expression.text;
+
+            //let extension =  `${parent.name} <|-- ${componentName}`;
+            //extendDefs.push(extension);
+
+            if(_extends === "Component") {
+              node.members.filter(prop => prop.kind === KIND_PROPERTY_DECLARATION || prop.kind === KIND_METHOD_DECLARATION)
+                .forEach(prop => {
+                  //console.log(prop.name.text);
+                  let keyName = prop.name.text;
+                  let isMethod = prop.kind === KIND_METHOD_DECLARATION;
+                  let isService = prop.decorators && prop.decorators[0].expression.text === "service";
+                  const scope = keyName.startsWith("_") ? "-" : "+";
+                  if(isMethod) {
+                    memberDefs.push(`${scope}${keyName}()`);
+                  } else if(isService) {
+                    let serviceDef =  `class ${prop.name.text} << (S, #FF7700) >>\n`;
+                    serviceDef += `${componentName} ..> ${prop.name.text} : service`;
+                    serviceDefs.push(serviceDef);
+                  }
+                  else {
+                    let type = prop.type ? kindMap[prop.type.kind] || 'any' : 'any';
+                    memberDefs.push(`${scope}${keyName} : ${type}`);
+                  }
 
 
+                });
+            }
+          }
+        }
 
+        break;
+    }
+    ts.forEachChild(node, traverse);
+
+  }
 }
 
 module.exports = transform;
---
+++ tests/transform-typescript.test.js
@@ -0,0 +1,106 @@
+const assert = require("assert");
+const transform = require("../src/transform-typescript");
+
+describe("transform typescript test: ", function() {
+
+  it("should generate transform public property properly", function() {
+
+    const input = `
+export default class MyClass extends Component{
+  prop1: boolean = true;
+}`;
+
+    const output = `
+@startuml
+
+
+class MyClass {
+  +prop1 : Boolean
+}
+
+@enduml`;
+
+    const uml = transform("my-class.ts", input, "MyClass");
+
+    assert.strictEqual(uml,output);
+  });
+
+  it("should generate multiple properties properly", function() {
+
+    const input = `
+export default class MyClass extends Component{
+  prop1: boolean =  true;
+  prop2: string  = 'hello';
+}`;
+
+    const output = `
+@startuml
+
+
+class MyClass {
+  +prop1 : Boolean
+  +prop2 : String
+}
+
+@enduml`;
+
+    const uml = transform("my-class.ts", input, "MyClass");
+
+    assert.strictEqual(uml,output);
+  });
+
+
+
+  it("should generate  public method properly", function() {
+
+    const input = `
+export default class MyClass extends Component{
+  method1() {
+  }
+}`;
+
+    const output = `
+@startuml
+
+
+class MyClass {
+  +method1()
+}
+
+@enduml`;
+
+    const uml = transform("my-class.ts", input, "MyClass");
+
+    assert.strictEqual(uml,output);
+  });
+
+
+it("should generate transform service properties  properly", function() {
+
+    const input = `
+export default class MyClass extends Component{
+  @service i18n!: I18N;
+  @service store!: DS.Store;
+}`;
+
+    const output = `
+@startuml
+
+class i18n << (S, #FF7700) >>
+MyClass ..> i18n : service
+class store << (S, #FF7700) >>
+MyClass ..> store : service
+class MyClass {
+  
+}
+
+@enduml`;
+
+    const uml = transform("my-class.ts", input, "MyClass");
+
+    assert.strictEqual(uml,output);
+  });
+
+
+
+});
