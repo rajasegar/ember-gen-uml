2687ff72aace24b17a251793d7e116c0a68a0a9f
Rajasegar <rajasegarc@freshdesk.com>
2019-11-07 12:05:46 +0530

    [CHORE] Upgrade dependencies

--- src/transform-typescript.js
+++ src/transform-typescript.js
@@ -5,24 +5,15 @@ const KIND_METHOD_DECLARATION = 157;
 
 const kindMap = {
   '124': 'Boolean',
-  '139': 'String'
+  '139': 'String',
 };
 
-
-
 function transform(fileName, code, componentName) {
-
   let memberDefs = [];
   let serviceDefs = [];
   let extendDefs = [];
 
-
-  const ast = ts.createSourceFile(
-    fileName, 
-    code, 
-    ts.ScriptTarget.ES2015,
-    /*setParentNodes */ true
-  );
+  const ast = ts.createSourceFile(fileName, code, ts.ScriptTarget.ES2015, /*setParentNodes */ true);
 
   //console.log(ast);
 
@@ -40,37 +31,39 @@ class ${componentName} {
   return umlData;
 
   function traverse(node) {
-    switch(node.kind) {
+    switch (node.kind) {
       case ts.SyntaxKind.ClassDeclaration:
         //console.log(node.heritageClauses[0].types[0].expression.escapedText);
-        if(node.heritageClauses[0]) {
-          if(node.heritageClauses[0].types[0]) {
+        if (node.heritageClauses[0]) {
+          if (node.heritageClauses[0].types[0]) {
             let _extends = node.heritageClauses[0].types[0].expression.text;
 
             //let extension =  `${parent.name} <|-- ${componentName}`;
             //extendDefs.push(extension);
 
-            if(_extends === "Component") {
-              node.members.filter(prop => prop.kind === KIND_PROPERTY_DECLARATION || prop.kind === KIND_METHOD_DECLARATION)
+            if (_extends === 'Component') {
+              node.members
+                .filter(
+                  prop =>
+                    prop.kind === KIND_PROPERTY_DECLARATION || prop.kind === KIND_METHOD_DECLARATION
+                )
                 .forEach(prop => {
                   //console.log(prop.name.text);
                   let keyName = prop.name.text;
                   let isMethod = prop.kind === KIND_METHOD_DECLARATION;
-                  let isService = prop.decorators && prop.decorators[0].expression.text === "service";
-                  const scope = keyName.startsWith("_") ? "-" : "+";
-                  if(isMethod) {
+                  let isService =
+                    prop.decorators && prop.decorators[0].expression.text === 'service';
+                  const scope = keyName.startsWith('_') ? '-' : '+';
+                  if (isMethod) {
                     memberDefs.push(`${scope}${keyName}()`);
-                  } else if(isService) {
-                    let serviceDef =  `class ${prop.name.text} << (S, #FF7700) >>\n`;
+                  } else if (isService) {
+                    let serviceDef = `class ${prop.name.text} << (S, #FF7700) >>\n`;
                     serviceDef += `${componentName} ..> ${prop.name.text} : service`;
                     serviceDefs.push(serviceDef);
-                  }
-                  else {
+                  } else {
                     let type = prop.type ? kindMap[prop.type.kind] || 'any' : 'any';
                     memberDefs.push(`${scope}${keyName} : ${type}`);
                   }
-
-
                 });
             }
           }
@@ -79,10 +72,7 @@ class ${componentName} {
         break;
     }
     ts.forEachChild(node, traverse);
-
   }
 }
 
 module.exports = transform;
-
-
