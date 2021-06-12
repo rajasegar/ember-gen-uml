cc936bc9b58cd17d8cfaedd523361dd5f15acd2e
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-06-12 13:24:40 +0530

    chore: upgrade deps, use jest for tests

--- src/transform.js
+++ src/transform.js
@@ -1,82 +1,77 @@
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const t = require('@babel/types');
+const debug = require('debug')('ember-gen-uml');
 
-function transform(code, componentName) {
+function transform(code) {
   const ast = parser.parse(code, {
     sourceType: 'module',
+    plugins: [['decorators-legacy', { decoratorsBeforeExport: false }]],
   });
 
   let umlData = '';
 
+  let memberDefs = [];
+  let serviceDefs = [];
+  let extendDefs = [];
+  let className = '';
   traverse(ast, {
     ExportDefaultDeclaration(p) {
-      if (
-        (t.isExportDefaultDeclaration(p.node), // eslint-disable-line
-        {
-          // eslint-disable-line
-          declaration: {
-            callee: {
-              object: { name: 'Component' },
-              property: { name: 'extend' },
-            },
-          },
-        })
-      ) {
-        const args = p.node.declaration.arguments;
-        let memberDefs = [];
-        let serviceDefs = [];
-        let extendDefs = [];
+      debugger;
+      if (p.node.declaration.type === 'ClassDeclaration') {
+        className = p.node.declaration.id.name;
+        const superClass = p.node.declaration.superClass.name;
 
-        if (args) {
-          const len = args.length;
+        let extension = `${superClass} <|-- ${className}`;
+        extendDefs.push(extension);
 
-          // extending other components or mixins
-          if (len > 1) {
-            const parentClasses = args.slice(0, len - 1);
-            parentClasses.forEach(parent => {
-              let extension = `${parent.name} <|-- ${componentName}`;
-              extendDefs.push(extension);
-            });
-          }
+        let props = p.node.declaration.body.body;
 
-          let props = args[len - 1].properties || [];
-
-          memberDefs = props.map(prop => {
-            let keyName = prop.key.type === 'StringLiteral' ? prop.key.value : prop.key.name;
+        memberDefs = props
+          .filter(prop => !prop.decorators)
+          .map(prop => {
+            let keyName = prop.key.name;
             const scope = keyName.startsWith('_') ? '-' : '+';
-            if (t.isObjectProperty(prop)) {
+            if (t.isClassProperty(prop)) {
               return `${scope}${keyName}`;
-            } else if (t.isObjectMethod(prop)) {
+            } else if (t.isClassMethod(prop)) {
               return `${scope}${keyName}()`;
             }
           });
 
-          props.forEach(prop => {
-            if (
-              prop.value &&
-              prop.value.type === 'CallExpression' &&
-              prop.value.callee.name === 'service'
-            ) {
-              let serviceDef = `class ${prop.key.name} << (S, #FF7700) >>\n`;
-              serviceDef += `${componentName} ..> ${prop.key.name} : service`;
-              serviceDefs.push(serviceDef);
-            }
-          });
-        }
+        props.forEach(prop => {
+          if (prop.decorators) {
+            const [decorator] = prop.decorators;
+            switch (decorator.expression.name) {
+              case 'service':
+                {
+                  let serviceDef = `class ${prop.key.name} << (S, #FF7700) >>\n`;
+                  serviceDef += `${className} ..> ${prop.key.name} : service`;
+                  serviceDefs.push(serviceDef);
+                }
+                break;
 
-        umlData = `
+              case 'attr':
+                memberDefs.push(`+${prop.key.name}`);
+                break;
+
+              default:
+                debug('Unknown decorator: ', decorator.expression.name);
+            }
+          }
+        });
+      }
+    },
+  });
+  umlData = `
 @startuml
 ${extendDefs.length > 0 ? extendDefs.join('\n') : ''}
 ${serviceDefs.length > 0 ? serviceDefs.join('\n') : ''}
-class ${componentName} {
+class ${className} {
   ${memberDefs.join('\n  ')}
 }
 
 @enduml`;
-      }
-    },
-  });
 
   return umlData;
 }
--- tests/transform.test.js
+++ tests/transform.test.js
@@ -1,179 +1,59 @@
-const assert = require("assert");
-const transform = require("../src/transform");
+/* globals test expect */
+const transform = require('../src/transform');
 
-describe("transform test: ", function() {
+test('should generate transform public property properly', function() {
+  const input = `
+export default class MyComponent extends Component {
+  prop1 = true;
+}`;
 
-  it("should generate transform public property properly", function() {
+  const uml = transform(input);
 
-    const input = `
-export default Component.extend({
-  prop1: true
-})`;
+  expect(uml).toMatchSnapshot();
+});
 
-    const output = `
-@startuml
+test('should generate transform multiple properties properly', function() {
+  const input = `
+export default class MyComponent extends Component {
+  prop1 =  true;
+  prop2 = 'hello';
+}`;
 
+  const uml = transform(input);
 
-class MyClass {
-  +prop1
-}
+  expect(uml).toMatchSnapshot();
+});
 
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-  it("should generate transform multiple properties properly", function() {
-
-    const input = `
-export default Component.extend({
-  prop1: true,
-  prop2: 'hello'
-})`;
-
-    const output = `
-@startuml
-
-
-class MyClass {
-  +prop1
-  +prop2
-}
-
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-
-  it("should generate transform private property properly", function() {
-
-    const input = `
-export default Component.extend({
-  _prop1: true
-})`;
-
-    const output = `
-@startuml
-
-
-class MyClass {
-  -_prop1
-}
-
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-
-  it("should generate transform public method properly", function() {
-
-    const input = `
-export default Component.extend({
+test('should generate transform public method properly', function() {
+  const input = `
+export default class MyComponent extends Component {
   method1() {
   }
-})`;
+}`;
 
-    const output = `
-@startuml
+  const uml = transform(input);
+  expect(uml).toMatchSnapshot();
+});
 
+test('should generate transform service properties  properly', function() {
+  const input = `
+export default class MyComponent extends Component {
+  @service marketplace;
+  @service overlay;
+}`;
 
-class MyClass {
-  +method1()
-}
+  const uml = transform(input);
+  expect(uml).toMatchSnapshot();
+});
 
-@enduml`;
+test('should generate model uml properly', function() {
+  const input = `
+export default class PersonModel extends Model {
+  @attr title;
+  @attr name;
+  @attr birthday;
+}`;
 
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-it("should generate transform private method properly", function() {
-
-    const input = `
-export default Component.extend({
-  _method1() {
-  }
-})`;
-
-    const output = `
-@startuml
-
-
-class MyClass {
-  -_method1()
-}
-
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-
-it("should generate transform service properties  properly", function() {
-
-    const input = `
-export default Component.extend({
-  marketplace: service(),
-  overlay: service()
-})`;
-
-    const output = `
-@startuml
-
-class marketplace << (S, #FF7700) >>
-MyClass ..> marketplace : service
-class overlay << (S, #FF7700) >>
-MyClass ..> overlay : service
-class MyClass {
-  +marketplace
-  +overlay
-}
-
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
-
-it("should generate transform extends properly", function() {
-
-    const input = `
-export default Component.extend(Component1, Mixin1, {
-  marketplace: service(),
-  overlay: service()
-})`;
-
-    const output = `
-@startuml
-Component1 <|-- MyClass
-Mixin1 <|-- MyClass
-class marketplace << (S, #FF7700) >>
-MyClass ..> marketplace : service
-class overlay << (S, #FF7700) >>
-MyClass ..> overlay : service
-class MyClass {
-  +marketplace
-  +overlay
-}
-
-@enduml`;
-
-    const uml = transform(input, "MyClass");
-
-    assert.strictEqual(uml,output);
-  });
-
+  const uml = transform(input);
+  expect(uml).toMatchSnapshot();
 });
